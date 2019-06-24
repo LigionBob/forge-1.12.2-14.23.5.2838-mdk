34e243bbbf6861239150dffd8faff9c1c3f17cc1
LigionBob <ligionbob@gmail.com>
2019-06-24 15:39:19 -0400

    Initial Commit

---
+++ src/main/java/ligionbob/cryingblocks/Main.java
@@ -0,0 +1,13 @@
+package ligionbob.cryingblocks;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MODID, version = Reference.MODVERSION, acceptedMinecraftVersions = Reference.MCVERSION, name = Reference.MODNAME)
+public class Main {
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+
+    }
+}
---
+++ src/main/java/ligionbob/cryingblocks/Reference.java
@@ -0,0 +1,9 @@
+package ligionbob.cryingblocks;
+
+public class Reference {
+    public static final String MODID = "cryingblocks";
+    public static final String MODNAME = "Crying Blocks";
+    public static final String MODVERSION = "0.0.1";
+    public static final String MCVERSION = "1.12.2";
+
+}
