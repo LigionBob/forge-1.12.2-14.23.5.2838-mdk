21e2f973df4c8a9b5c5871172062e78564335297
LigionBob <ligionbob@gmail.com>
2019-06-25 18:26:03 -0400

    textures

---
+++ src/main/java/ligionbob/cryingblocks/utils/handlers/RegistryHandler.java
@@ -0,0 +1,25 @@
+package ligionbob.cryingblocks.utils.handlers;
+
+import ligionbob.cryingblocks.init.ModItems;
+import ligionbob.cryingblocks.utils.IHasModel;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod.EventBusSubscriber
+public class RegistryHandler {
+    @SubscribeEvent
+    public static void onItemRegister(RegistryEvent.Register<Item> event) {
+        event.getRegistry().registerAll(ModItems.ITEMS.toArray(new Item[0]));
+    }
+    @SubscribeEvent
+    public static void onModelRegister(ModelRegistryEvent event) {
+        for(Item item : ModItems.ITEMS) {
+            if(item instanceof IHasModel) {
+                ((IHasModel)item).registerModels();
+            }
+        }
+    }
+}
